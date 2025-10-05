efff7ed64afae011d5767e75b4dd41a5f36a1064
Wenny <shine5983@gmail.com>
2025-10-05 14:22:23 +0800

    refactor(Sidebar): refactor sidebar

---
+++ src/components/Sidebar/MenuItem.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+
+interface MenuItemProps {
+  item: {
+    name: string;
+    href: string;
+  };
+  onClose: () => void;
+}
+
+const MenuItem = ({ item, onClose }: MenuItemProps) => {
+  const isAnchor = item.href.startsWith("#");
+  return isAnchor ? (
+    <a
+      href={item.href}
+      className="px-6 py-4 hover:bg-gray-700 dark:hover:bg-gray-800 border-b border-[#434343]"
+    >
+      {item.name}
+    </a>
+  ) : (
+    <Link
+      href={item.href}
+      className="px-6 py-4 hover:bg-gray-700 dark:hover:bg-gray-800 border-b border-[#434343]"
+      onClick={onClose}
+    >
+      {item.name}
+    </Link>
+  );
+};
+
+export default MenuItem;

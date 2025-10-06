05ec785f211988b50da3ce227a1d37c25c4d0763
Lindsay <lindsay@giloo.ist>
2025-10-06 12:44:29 +0800

    update title meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "花蓮援助平台",
+  title: "光復超人",
   description: "提供花蓮地區災害援助資訊、志工招募與災民協助服務",
   icons: {
     icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏔️</text></svg>",

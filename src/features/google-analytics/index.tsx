674cf168468b5cf3c9c006ff34527fe23881ba24
Lindsay <lindsay@giloo.ist>
2025-10-07 22:50:23 +0800

    feat(ga): prevent no env situation

--- src/features/google-analytics/index.tsx
+++ src/features/google-analytics/index.tsx
@@ -5,8 +5,12 @@ import ReactGA from "react-ga4";
 import { env } from "@/config/env";
 
 export function GoogleAnalytics() {
+  const isProd = process.env.NODE_ENV === "production";
+
   useEffect(() => {
+    if (!isProd || !env.NEXT_PUBLIC_GA4_ID) return;
     ReactGA.initialize(env.NEXT_PUBLIC_GA4_ID);
-  }, []);
+  }, [isProd]);
+
   return null;
 }

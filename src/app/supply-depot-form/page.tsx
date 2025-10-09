3abc790c02350432341920e4f0adf04c9e18541b
juliawang <juli135793@gmail.com>
2025-10-09 15:50:26 +0800

    style(supply-depot-form): remove unnecessary import

--- src/app/supply-depot-form/page.tsx
+++ src/app/supply-depot-form/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useRef, useState } from "react";
-import { motion, supportsPartialKeyframes } from "framer-motion";
+import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
 import DropdownSelect from "@/components/DropdownSelect";
 

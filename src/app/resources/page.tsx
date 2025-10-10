949864dcad67c3cf8f1e4674e21278944f5119fa
Oswald <yzdk72@gmail.com>
2025-10-10 17:27:01 +0800

    add ga

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -11,6 +11,23 @@ export const metadata: Metadata = {
 export default function ResourcesPage() {
   return (
     <Wrapper hideFooter>
+      {/* 插入 GA4 Script */}
+      <script
+        src="https://www.googletagmanager.com/gtag/js?id=G-G71PVV5FTV"
+        strategy="afterInteractive"
+      />
+      <script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-G71PVV5FTV', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </script>
+
+
       <iframe
         src="https://pinkowo.github.io/hualien-bees/"
         className="w-full border-0"
--- src/app/volunteer-register/page.tsx
+++ src/app/volunteer-register/page.tsx
@@ -11,6 +11,21 @@ export const metadata: Metadata = {
 export default function VolunteerRegisterPage() {
   return (
     <Wrapper hideFooter>
+        {/* 插入 GA4 Script */}
+      <script
+        src="https://www.googletagmanager.com/gtag/js?id=G-G71PVV5FTV"
+        strategy="afterInteractive"
+      />
+      <script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-G71PVV5FTV', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </script>
       <iframe
         src="https://hualien-volunteers-frontend-iota.vercel.app/"
         className="w-full border-0"
